7a0eb59be6b8d5e04d8c3fe4ffb743a3ce0d0e42
Chino Young <chino.young@goabroad.com>
2023-11-09 16:18:24 +0800

    Edits

--- app/products/add-product.js
+++ app/products/add-product.js
@@ -64,17 +64,17 @@ export default function AddProduct() {
             <AiOutlineRight className="ml-auto ui-open:rotate-90 ui-open:transform" />
           </Disclosure.Button>
           <Disclosure.Panel>
-            <form>
-              <FlexCol>
-                <input
-                  value={newItem.name}
-                  onChange={(e) =>
-                    setNewItem({ ...newItem, name: e.target.value })
-                  }
-                  className="text-sm border w-full px-5 py-2.5 rounded-md"
-                  type="text"
-                  placeholder="Name"
-                />
+            <FlexCol>
+              <input
+                value={newItem.name}
+                onChange={(e) =>
+                  setNewItem({ ...newItem, name: e.target.value })
+                }
+                className="text-sm border w-full px-5 py-2.5 rounded-md"
+                type="text"
+                placeholder="Name"
+              />
+              <div className="grid grid-cols-2 gap-4">
                 <input
                   value={newItem.price}
                   onChange={(e) =>
@@ -111,14 +111,14 @@ export default function AddProduct() {
                   type="text"
                   placeholder="Image"
                 />
-                <button
-                  onClick={addItem}
-                  className="rounded-md px-5 py-2.5 text-white text-center text-xs bg-red-500"
-                >
-                  Add
-                </button>
-              </FlexCol>
-            </form>
+              </div>
+              <button
+                onClick={addItem}
+                className="rounded-md px-5 py-2.5 text-white text-center text-xs bg-red-500"
+              >
+                Add
+              </button>
+            </FlexCol>
           </Disclosure.Panel>
         </FlexCol>
       </Disclosure>
--- app/products/products.js
+++ app/products/products.js
@@ -1,12 +1,13 @@
 "use client";
 import React, { useState, useContext } from "react";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase";
 import { Disclosure } from "@headlessui/react";
 import { AiOutlineRight } from "react-icons/ai";
 import Card from "../layout/card";
 import Button from "../layout/button";
 import SubHeading from "../layout/sub-heading";
+import FlexCol from "../layout/flex-col";
 import Confirmation from "../components/confirmation";
 import { ItemsContext } from "../context/items-context";
 
@@ -37,7 +38,7 @@ export default function Products() {
                 <li
                   key={index}
                   className={`flex flex-col items-start gap-2 p-2 rounded-md ${
-                    item.stock < 1 ? "bg-red-50" : "bg-slate100"
+                    item.stock < 1 ? "bg-slate-100" : "bg-slate100"
                   }`}
                 >
                   <Disclosure>
@@ -64,7 +65,7 @@ export default function Products() {
                       <AiOutlineRight className="ml-auto ui-open:rotate-90 ui-open:transform" />
                     </Disclosure.Button>
                     <Disclosure.Panel className="w-full rounded-md flex flex-col justify-end">
-                      <div className="flex flex-col gap-2">
+                      <div className="flex flex-col gap-4">
                         <div className="flex justify-between text-xs text-slate-800 p-4 bg-slate-200 rounded-md">
                           <span>
                             Price: <strong>₱{item.price}</strong>
@@ -77,7 +78,11 @@ export default function Products() {
                           </span>
                         </div>
                         <div className="flex self-end gap-2">
-                          <Button label="Edit" secondary small />
+                          {/* <Button
+                            label="Edit"
+                            secondary
+                            small
+                          /> */}
                           <Button
                             label="Delete"
                             small
